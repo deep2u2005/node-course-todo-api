c68ba1cb9556b27c69b6b58e84ada3a8293dcc14
deep2u2005 <deep2u2005@gmail.com>
2018-06-01 18:53:50 +0530

    MONGODB_URI config change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var mongoEnv = process.env.MONGODB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp';
+var mongoEnv = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.connect(mongoEnv);
 console.log(mongoEnv);
 module.exports = {
